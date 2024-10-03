10eae706572906727ae97f730ac729f403330d90
Roger Chao <rogman@gmail.com>
2024-10-03 16:49:22 -0700

    templates

---
+++ docker-with-docker-bridge/main.tf
@@ -0,0 +1,179 @@
+terraform {
+  required_providers {
+    coder = {
+      source  = "coder/coder"
+    }
+    docker = {
+      source  = "kreuzwerker/docker"
+    } 
+  }
+}
+
+provider "docker" {
+  host = var.socket
+}
+
+variable "socket" {
+  type        = string
+  description = <<-EOF
+  The Unix socket that the Docker daemon listens on and how containers
+  communicate with the Docker daemon.
+
+  Either Unix or TCP
+  e.g., unix:///var/run/docker.sock
+
+  EOF
+  default = "unix:///var/run/docker.sock"
+}
+
+data "coder_workspace" "me" {
+}
+
+data "coder_workspace_owner" "me" {
+}
+
+data "coder_parameter" "dotfiles_url" {
+  name        = "Dotfiles URL (optional)"
+  description = "Personalize your workspace e.g., https://github.com/coder/example-dotfiles.git"
+  type        = "string"
+  default     = ""
+  mutable     = true 
+  icon        = "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png"
+  order       = 1
+}
+
+resource "coder_agent" "coder" {
+  os   = "linux"
+  arch = "amd64"
+
+  # The following metadata blocks are optional. They are used to display
+  # information about your workspace in the dashboard. You can remove them
+  # if you don't want to display any information.
+  # For basic resources, you can use the `coder stat` command.
+  # If you need more control, you can write your own script.
+  metadata {
+    display_name = "CPU Usage"
+    key          = "0_cpu_usage"
+    script       = "coder stat cpu"
+    interval     = 10
+    timeout      = 1
+  }
+
+  metadata {
+    display_name = "RAM Usage"
+    key          = "1_ram_usage"
+    script       = "coder stat mem"
+    interval     = 10
+    timeout      = 1
+  }
+
+  metadata {
+    display_name = "Home Disk"
+    key          = "3_home_disk"
+    script       = "coder stat disk --path $${HOME}"
+    interval     = 60
+    timeout      = 1
+  }
+
+
+  display_apps {
+    vscode = true
+    vscode_insiders = false
+    ssh_helper = false
+    port_forwarding_helper = true
+    web_terminal = true
+  }
+
+  startup_script_behavior = "non-blocking"
+  connection_timeout = 300 
+
+
+  dir = "/home/coder"
+  startup_script = <<EOT
+#!/bin/sh
+
+# clone repo
+if [ ! -d "flask-redis-docker-compose" ]; then
+  git clone --progress https://github.com/coder/flask-redis-docker-compose.git 
+fi
+
+# use coder CLI to clone and install dotfiles
+if [ ! -z "${data.coder_parameter.dotfiles_url.value}" ]; then
+  coder dotfiles -y ${data.coder_parameter.dotfiles_url.value}
+fi
+
+# install and start code-server
+curl -fsSL https://code-server.dev/install.sh | sh
+code-server --auth none --port 13337 >/dev/null 2>&1 &
+
+  EOT  
+}
+
+# code-server
+resource "coder_app" "code-server" {
+  agent_id      = coder_agent.coder.id
+  slug          = "cs"  
+  display_name  = "code-server"
+  icon          = "/icon/code.svg"
+  url           = "http://localhost:13337?folder=/home/coder"
+  subdomain = false
+  share     = "owner"
+
+  healthcheck {
+    url       = "http://localhost:13337/healthz"
+    interval  = 3
+    threshold = 10
+  }  
+}
+
+resource "docker_network" "private_network" {
+  name = "network-${data.coder_workspace.me.id}"
+  driver = "bridge"
+}
+
+resource "docker_container" "dind" {  
+  count   = data.coder_workspace.me.start_count  
+  image      = "docker:dind"
+  privileged = true
+  name       = "dind-${data.coder_workspace.me.id}"
+  entrypoint = ["dockerd", "-H", "tcp://0.0.0.0:2375"]
+  networks_advanced {
+    name = docker_network.private_network.name
+  }
+}
+
+resource "docker_container" "workspace" {
+  count   = data.coder_workspace.me.start_count
+  image   = "codercom/enterprise-base:ubuntu"
+  name    = "dev-${data.coder_workspace.me.id}"
+  command = ["sh", "-c", coder_agent.coder.init_script]
+  env = [
+    "CODER_AGENT_TOKEN=${coder_agent.coder.token}",
+    "DOCKER_HOST=${docker_container.dind[0].name}:2375"
+  ]
+  volumes {
+    container_path = "/home/coder/"
+    volume_name    = docker_volume.coder_volume.name
+    read_only      = false
+  }    
+  networks_advanced {
+    name = docker_network.private_network.name
+  }
+}
+
+resource "docker_volume" "coder_volume" {
+  name = "coder-${data.coder_workspace_owner.me.name}-${data.coder_workspace.me.name}"
+}
+
+resource "coder_metadata" "workspace_info" {
+  count       = data.coder_workspace.me.start_count
+  resource_id = docker_container.workspace[0].id 
+  item {
+    key   = "image"
+    value = "docker.io/codercom/enterprise-base:ubuntu"
+  }
+  item {
+    key   = "repo cloned"
+    value = "https://github.com/coder/flask-redis-docker-compose.git"
+  }     
+}
